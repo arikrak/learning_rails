3ff59d172f697db0de4a4c8dff48fbac376b4c7c
Ariel Krakowski <arikrak@email.com>
2012-06-07 16:13:00 -0400

    tryingrubymine - initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LEARNRAILS612::Application.config.secret_token = 'c9efaf0703692f783b805e29b680446eb97b9b9dd96505803464bc3b84b80cc9d94699a3146c50ee1d8d48e1e584d92bb4c833bed3e694f90bdbecbe1c25a1eb'
